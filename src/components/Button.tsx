77ec9f9be999b3b93d3de33f4f1055bf37e4011f
Justin White <justin.gj.white@gmail.com>
2026-01-28 07:42:41 -0500

    Update all partner links to /start endpoint

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<ButtonProps> = ({ children, className = '', variant = 'de
   
   return (
     <a 
-      href="https://get.bambookitchens.io/" 
+      href="https://get.bambookitchens.io/start" 
       target="_blank" 
       rel="noopener noreferrer"
       className={`${buttonClass} inline-block px-6 py-4 rounded font-normal ${className}`}
